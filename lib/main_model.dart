647f95e04c65241db9fef7c4bb150e782b80812b
Aoshima <>
2021-01-19 20:28:24 +0900

    firestoreからデータを取得してlistenerに通知するmodel

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,3 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MainModel extends ChangeNotifier {}
+class MainModel extends ChangeNotifier {
+  List<Todo> todoList = [];
+
+  Future getTodoList() async {
+    final snapshot =
+        await FirebaseFirestore.instance.collection('todoList').get();
+    final docs = snapshot.docs;
+    this.todoList = docs.map((doc) => Todo(doc)).toList();
+    notifyListeners();
+  }
+}
+
+class Todo {
+  Todo(DocumentSnapshot doc) {
+    this.title = doc.data()['title'];
+  }
+
+  String title;
+  DateTime createdAt;
+}
